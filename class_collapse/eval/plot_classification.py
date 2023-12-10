19d4c6ea1f7fb4673de1f2093e8e154a4083f55e
Antoine Poirier <antoine.poirier@etu.minesparis.psl.eu>
2023-12-10 23:13:22 +0100

    update plots

--- class_collapse/eval/plot_classification.py
+++ class_collapse/eval/plot_classification.py
@@ -17,21 +17,21 @@ def plot_classification(config, model, data, comment):
 
     plt.subplot(2,2,1)
     plt.scatter(data_emb[:,0], data_emb[:,1], c=data.y_test_fine, cmap='flag')
-    plt.title("Fine clusters")
+    plt.title("Fine clusters", fontsize=20)
 
     plt.subplot(2,2,2)
     plt.scatter(data_emb[:,0], data_emb[:,1], c=data.y_test, cmap='flag')
-    plt.title("Coarse clusters")
+    plt.title("Coarse clusters", fontsize=20)
 
     data.X_test_embs, y_pred_fine, y_pred_coarse = knn.predict()
     plt.subplot(2,2,3)
     plt.scatter(data.X_test_embs[:,0], data.X_test_embs[:,1], c=y_pred_fine, cmap='flag')
-    plt.title(f"KNN accuracy: {np.mean(y_pred_fine == knn.data.y_test_fine):.3f} ({comment})")
+    plt.title(f"KNN accuracy: {np.mean(y_pred_fine == knn.data.y_test_fine):.3f} ({comment})", fontsize=20)
 
 
     plt.subplot(2,2,4)
     plt.scatter(data.X_test_embs[:,0], data.X_test_embs[:,1], c=y_pred_coarse, cmap='flag')    
-    plt.title(f"KNN accuracy: {np.mean(y_pred_coarse == knn.data.y_test):.3f} ({comment})")
+    plt.title(f"KNN accuracy: {np.mean(y_pred_coarse == knn.data.y_test):.3f} ({comment})", fontsize=20)
 
     plt.tight_layout()
     plt.savefig(f"clusters_{comment}.png")
