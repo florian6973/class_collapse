186cb2167e7583c8ddbeb39a6b025a91bd864b36
Florent Pollet <flo1.raymond@gmail.com>
2023-12-10 11:29:51 -0500

    clean configs

--- scripts/test_nce.py
+++ scripts/test_nce.py
@@ -11,16 +11,32 @@ test_tensor = torch.Tensor([
 labels = torch.Tensor([0, 1, 1, 2])
 print(loss(test_tensor, labels))
 
+def infonce2(x, labels, temperature=0.1):
+    embeddings = x
+    num = torch.matmul(embeddings, embeddings.T) / temperature
+
+    numerator_spread = -1 * torch.diagonal(num, 0)
+
+    # Use torch.gather to index and select relevant elements
+    mask = labels.unsqueeze(1) == labels  # Create a boolean mask
+    denominator_spread = torch.logsumexp(num[mask], dim=0)
+
+
+    log_prob_spread = numerator_spread + denominator_spread
+
+    return log_prob_spread.mean()
+print(infonce2(test_tensor, labels))
+
 def infonce(x, labels, temperature=0.1):
     num = torch.matmul(x, x.T) / temperature
     print(num.shape)
     numerator_spread = -1 * torch.diagonal(num, 0)
     print(numerator_spread.shape)
+    print("numerator", num)
+    print(torch.logsumexp(num[1][labels==labels[1]], 0))
     denominator_spread = torch.stack(
         [
-            # reshape num with an extra dimension,
-            # then take the sum over everything
-            torch.logsumexp(num[i][labels==labels[i]], 0).sum()
+            torch.logsumexp(num[i][labels==labels[i]], 0)
             for i in range(len(x))
         ]
     )
